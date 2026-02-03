fc4690acb3a91e32dcb4790ff0a01a0cbbf04819
suhas <srisuhasgowda24@gmail.com>
2026-02-03 12:47:50 +0530

    Assignment Day-2

---
+++ 2.py
@@ -0,0 +1,85 @@
+# num = int(input("Enter the number:"))
+# print(num)
+# print(type(num))
+
+
+
+# Printing the type of variable 
+# c=-1
+# print(type(c))
+# d=5.7
+# print(type(d))
+
+# a=False
+# a=false
+# print(type(a))
+
+
+#Operators
+# x=-5
+# y=-10
+# print(x+y)
+# print(x-y)
+# print(x/y)
+# print(x*y)
+# print(x==y)
+
+
+# x="india"
+# y="indi"
+# print(x==y)
+
+
+# x=10
+# y=30
+# print(x+y, x-y, x/y, x*y, x==y)
+
+# Asssigning String to a variable
+# name ="suhas"
+# rollno=113
+# college="KSSEM" 
+# Hobby="adventure"
+# Goal="coding"
+# print(name, rollno, college, Hobby, Goal )
+
+# marks=90
+# print(marks)
+# marks=70
+# print(marks)
+
+# Declaring a function
+# tan_1 = 1
+# tan_1 = "True"
+# print(tan_1)
+
+# a="2"
+# 1_b="True"
+
+#123="No"
+# a bc="error"
+
+
+# _a=123
+# bai@="hi"
+
+
+# 1a = "abc"
+# print(1a)
+
+
+
+# if loops 
+# num = 11
+# if num > 1:
+#     print("Number is Positive")
+#     if num >= 10:
+#         print("Number is Equal/Greater then 10")
+ 
+
+
+# if else loop
+# age = 18
+# if age<18:
+#     print("Not eligible to vote")
+# else:
+#     print("Eligible to vote")
